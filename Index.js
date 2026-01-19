defb5fcb3258fa2b6db8674228b84b0962552c4a
saamuka01 <jajabrasilb@gmail.com>
2026-01-19 09:52:38 -0300

    Initialize Discord bot with message handling

---
+++ Index.js
@@ -0,0 +1,23 @@
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`🤖 Bot ligado como ${client.user.tag}`);
+});
+
+client.on("messageCreate", msg => {
+  if (msg.author.bot) return;
+
+  if (msg.content === "!ping") {
+    msg.reply("🏓 Pong!");
+  }
+});
+
+client.login(process.env.TOKEN);
